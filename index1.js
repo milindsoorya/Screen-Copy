63c19720288edc26ca7a97a7c9ab320fd03a267f
milind soorya <36722289+milindsoorya@users.noreply.github.com>
2020-11-04 21:31:25 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -42,7 +42,7 @@ app.post('/', async (req, res) => {
       try {
         const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'domcontentloaded' });
+        await page.goto(url, { waitUntil: 'networkidle2' });
         const screenshot = await page.screenshot({
           fullPage: req.body.fullPage ? true : false,
           type: `${req.body.fileType}`,
